132d05be2fc26f829116512664758b1e7d161afc
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-10-20 13:56:24 +0300

    templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hello")
+    return render(request, 'home.html')
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'room.html')
